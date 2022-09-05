91e195fe380531b44c309fd0e555c1218218d543
Vasyalvv <vasyalvv@gmail.com>
2022-09-05 20:03:11 +0500

    DTO модели игры, издателя, жанра

---
+++ GameStore.Domain/DTO/GameDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.DTO
+{
+    public class GameDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public PublisherDTO Publisher { get; set; }
+
+        public IList<GenreDTO> Genres { get; set; }
+    }
+}
