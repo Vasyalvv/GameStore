94a784e44fe157ec0d0d0ca2fa53b4e0c35966cf
Vasyalvv <vasyalvv@gmail.com>
2022-09-05 12:34:49 +0500

    Базовые классы сущностей

---
+++ GameStore.Domain/Inerfaces/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Inerfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
